25e64ccca23f2e7722a59a7379112a539688e0e0
ajjohnson <aj132@icloud.com>
2023-07-12 11:46:16 -0500

    Update Workout Center

---
+++ Server/Controllers/WorkoutController.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProServ.Shared.Models;
+using ProServ.Shared.Models.NET_CORE_USER;
+using ProServ.Shared.Models.UserInfo;
+using Microsoft.IdentityModel.Tokens;
+using ProServ.Server.Contexts;
+using Microsoft.EntityFrameworkCore;
+using ProServ.Shared.Models.Coaches;
+using System.Diagnostics;
+using System.Globalization;
+using ProServ.Shared.Models.Workouts;
+
+namespace ProServ.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkoutController : ControllerBase
+    {
+        private readonly IDbContextFactory<ProServDbContext> _contextFactory;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public WorkoutController(IDbContextFactory<ProServDbContext> contextFactory, UserManager<IdentityUser> userManager)
+        {
+            _contextFactory = contextFactory;
+            _userManager = userManager;
+        }
+
+        [HttpGet("add-workout")]
+        [Authorize]
+        public async Task<ActionResult> AddNewWorkout(Workout newWorkout)
+        {
+            try
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return NotFound("User not found");
+                }
+
+                if (user.Id != null)
+                {
+                    newWorkout.CoachId = user.Id;
+                }
+                else
+                {
+                    return NotFound("User ID not found");
+                }
+
+                var db = _contextFactory.CreateDbContext();
+                if (db == null)
+                {
+                    return StatusCode(500, "Database connection failed");
+                }
+
+                await db.Workouts.AddAsync(newWorkout);
+                await db.SaveChangesAsync();
+
+                //Now Insert parameters and blocks
+                var blocks = newWorkout.WorkoutBlocks;
+                foreach (var block in blocks)
+                {
+                    block.WorkoutId = newWorkout.WorkoutId;
+                    await db.WorkoutBlocks.AddAsync(block);
+                    await db.SaveChangesAsync();
+
+                    var parameters = block.Parameters;
+                    foreach (var parameter in parameters)
+                    {
+                        parameter.BlockId = block.BlockId;
+                        await db.Parameters.AddAsync(parameter);
+                        await db.SaveChangesAsync();
+                    }
+                }
+
+                return Ok("New Workout Created");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
